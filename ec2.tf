7e5721f1c4c52ee001882ceae48a8d441cfc588b
archi <jainarchi20304@gmail.com>
2026-04-19 14:00:27 +0530

    Initial Terraform project

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "expense-tracker-instance"
+
+  instance_type = "t3.micro"
+  key_name      = "project"
+  monitoring    = true
+  subnet_id     = "subnet-026217194928dec19"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
